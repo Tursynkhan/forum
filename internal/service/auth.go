ea3867eeba1dfa89d09bd507d389763da639563d
tursynkhan <tursynkhan001@gmail.com>
2022-12-19 21:43:15 +0600

    Authservice

---
+++ internal/service/auth.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"crypto/sha1"
+	"fmt"
+
+	"forum/internal/models"
+	"forum/internal/repository"
+)
+
+const salt = "hkhasdfa2454654asdf1asdf4a5sdf"
+
+type AuthService struct {
+	repo repository.Autorization
+}
+
+func NewAuthService(repo repository.Autorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user models.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
