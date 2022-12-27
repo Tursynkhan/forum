94f4e5d4473a63b7dba7da4cbe678a055cc054f2
tursynkhan <tursynkhan001@gmail.com>
2022-12-27 20:53:51 +0600

    add check function

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -4,6 +4,8 @@ import (
 	"crypto/sha1"
 	"errors"
 	"fmt"
+	"log"
+	"net/mail"
 	"time"
 
 	"forum/internal/models"
@@ -24,15 +26,28 @@ func NewAuthService(repo repository.Autorization) *AuthService {
 }
 
 func (s *AuthService) CreateUser(user models.User) error {
+	userCheck, err := s.repo.GetUser(user.Username)
+	if err != nil {
+		log.Println(err)
+	}
+	if userCheck.Username == user.Username {
+		return errors.New("User exist")
+	}
+	if err := checkUser(user); err != nil {
+		return err
+	}
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
 }
 
 func (s *AuthService) GenerateToken(username, password string) (string, time.Time, error) {
-	// get user from DB
 	user, err := s.repo.GetUser(username)
+	log.Println(user)
 	if err != nil {
-		return "", time.Time{}, errors.New("User dont exist")
+		return "", time.Time{}, errors.New("User don't exist")
+	}
+	if user.Username == "" {
+		return "", time.Time{}, errors.New("User don't exist")
 	}
 	// if err := checkHash(user.Password, password); err != nil {
 	// 	return "", time.Time{}, errors.New("Password doesn't match")
@@ -55,4 +70,22 @@ func generatePasswordHash(password string) string {
 
 func checkHash(hpass, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hpass), []byte(password))
+}
+
+func checkUser(user models.User) error {
+	for _, letter := range user.Username {
+		if letter < 32 || letter > 126 {
+			return errors.New("Incorrect input")
+		}
+	}
+	if _, err := mail.ParseAddress(user.Email); err != nil {
+		return errors.New("Invalid email")
+	}
+	if len(user.Username) < 2 || len(user.Username) > 36 {
+		return errors.New("Invalid username")
+	}
+	if user.Password != user.RepeatPassword {
+		return errors.New("Password doesn't match")
+	}
+	return nil
 }
