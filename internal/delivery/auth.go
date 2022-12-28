348b1dc1b88bf357339bf99635588c627604c54b
tursynkhan <tursynkhan001@gmail.com>
2022-12-28 20:46:25 +0600

    handle error

--- internal/delivery/auth.go
+++ internal/delivery/auth.go
@@ -10,18 +10,22 @@ import (
 
 func (h *Handler) signUp(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/auth/signup" {
-		h.ErrorHandler(w, r, errStatus{http.StatusNotFound, http.StatusText(http.StatusNotFound)})
+		log.Println("Sign Up:Wrong URL Path")
+		h.errorHandler(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		return
 	}
 	if r.Method == "GET" {
 		ts, err := template.ParseFiles("./ui/templates/signUp.html")
 		if err != nil {
-			h.ErrorHandler(w, r, errStatus{http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)})
+			log.Printf("Sign Up: Execute:%v", err)
+			h.errorHandler(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 		err = ts.Execute(w, nil)
 		if err != nil {
 			log.Println(err.Error())
-			h.ErrorHandler(w, r, errStatus{http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)})
+			h.errorHandler(w, http.StatusInternalServerError, err.Error())
+			return
 		}
 	} else if r.Method == "POST" {
 
@@ -37,36 +41,42 @@ func (h *Handler) signUp(w http.ResponseWriter, r *http.Request) {
 			RepeatPassword: pswRepeat,
 		}
 		if err := h.services.Autorization.CreateUser(newUser); err != nil {
-			log.Println(err)
-			h.ErrorHandler(w, r, errStatus{http.StatusBadRequest, http.StatusText(http.StatusBadRequest)})
+			log.Printf("Sign Up: Create User: %v", err)
+			h.errorHandler(w, http.StatusForbidden, http.StatusText(http.StatusForbidden))
 			return
 		}
-		http.Redirect(w, r, "/auth/signin", 303)
+		http.Redirect(w, r, "/auth/signin", 301)
+	} else {
+		log.Println("Sign Up: Method not allowed")
+		h.errorHandler(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed))
+		return
 	}
-}
-
-type signInInput struct {
-	Username string
-	Password string
 }
 
 func (h *Handler) signIn(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/auth/signin" {
-		h.ErrorHandler(w, r, errStatus{http.StatusNotFound, http.StatusText(http.StatusNotFound)})
+		log.Println("Sign In:Wrong URL Path")
+		h.errorHandler(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		return
 	}
 	if r.Method == "GET" {
 		ts, err := template.ParseFiles("./ui/templates/signIn.html")
 		if err != nil {
-			h.ErrorHandler(w, r, errStatus{http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)})
+			h.errorHandler(w, http.StatusInternalServerError, err.Error())
+			return
 		}
-		err = ts.Execute(w, nil)
+		if err = ts.Execute(w, nil); err != nil {
+			log.Printf("Sign In: Execute:%v", err)
+			h.errorHandler(w, http.StatusInternalServerError, err.Error())
+			return
+		}
 	} else if r.Method == "POST" {
 		name := r.FormValue("name")
-		email := r.FormValue("email")
-		sessionToken, expiresTime, err := h.services.GenerateToken(name, email)
+		psw := r.FormValue("psw")
+		sessionToken, expiresTime, err := h.services.GenerateToken(name, psw)
 		if err != nil {
-			log.Println(err)
-			h.ErrorHandler(w, r, errStatus{http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)})
+			log.Printf("Sign In: Generate Token:%v", err)
+			h.errorHandler(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 		http.SetCookie(w, &http.Cookie{
