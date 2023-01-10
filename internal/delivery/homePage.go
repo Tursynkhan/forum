0eb031a9e0931fb8a8009cd1c204b1529bdad50d
tursynkhan <tursynkhan001@gmail.com>
2023-01-10 18:51:05 +0600

    get all post in repository

--- internal/delivery/homePage.go
+++ internal/delivery/homePage.go
@@ -9,15 +9,8 @@ import (
 )
 
 func (h *Handler) home(w http.ResponseWriter, r *http.Request) {
-	user, ok := r.Context().Value(key).(models.User)
-	if !ok {
-		ts, err := template.ParseFiles("./ui/templates/index.html")
-		if err = ts.Execute(w, nil); err != nil {
-			h.errorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-			return
-		}
-		return
-	}
+	var posts []models.Post
+	var err error
 	if r.URL.Path != "/" {
 		log.Println("home page: wrong url")
 		h.errorHandler(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
@@ -28,13 +21,34 @@ func (h *Handler) home(w http.ResponseWriter, r *http.Request) {
 		h.errorHandler(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed))
 		return
 	}
+	user, ok := r.Context().Value(key).(models.User)
+	if !ok {
+		ts, err := template.ParseFiles("./ui/templates/index.html")
+		if err = ts.Execute(w, nil); err != nil {
+			log.Printf("homepage: execute: %v", err)
+			h.errorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+			return
+		}
+		return
+	}
+	posts, err = h.services.GetAllPosts()
+	if err != nil {
+		log.Println("home page: get all posts", err)
+		h.errorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+	info := models.Info{
+		Posts: posts,
+		User:  user,
+	}
 	ts, err := template.ParseFiles("./ui/templates/index.html")
 	if err != nil {
 		log.Println(err.Error())
 		h.errorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
-	if err = ts.Execute(w, user); err != nil {
+	if err = ts.Execute(w, info); err != nil {
+		log.Println("home page: execute error:", err)
 		h.errorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
