762e6ba691820b868f96e7b438bd8d651f3bb97d
tursynkhan <tursynkhan001@gmail.com>
2023-01-11 20:36:30 +0600

    little change

--- internal/repository/post.go
+++ internal/repository/post.go
@@ -14,7 +14,7 @@ type PostRepository struct {
 type Post interface {
 	CreatePost(post models.Post) error
 	GetAllPosts() ([]models.Post, error)
-	GetPost(title string) (models.Post, error)
+	GetPost(id int) (models.Post, error)
 }
 
 func NewPostRepository(db *sql.DB) *PostRepository {
@@ -30,14 +30,14 @@ func (r *PostRepository) CreatePost(post models.Post) error {
 }
 
 func (r *PostRepository) GetAllPosts() ([]models.Post, error) {
-	rows, err := r.db.Query("SELECT Title,Content from posts")
+	rows, err := r.db.Query("SELECT Id,Title,Content from posts")
 	if err != nil {
 		return []models.Post{}, fmt.Errorf("repository: get all posts: %w", err)
 	}
 	var posts []models.Post
 	for rows.Next() {
 		p := models.Post{}
-		err := rows.Scan(&p.Title, &p.Content)
+		err := rows.Scan(&p.ID, &p.Title, &p.Content)
 		if err == sql.ErrNoRows {
 			return []models.Post{}, errors.New("No posts")
 		} else if err != nil {
@@ -48,20 +48,19 @@ func (r *PostRepository) GetAllPosts() ([]models.Post, error) {
 	return posts, nil
 }
 
-func (r *PostRepository) GetPost(title string) (models.Post, error) {
-	rows, err := r.db.Query("SELECT Title,Content from posts WHERE Title=$1", title)
+func (r *PostRepository) GetPost(id int) (models.Post, error) {
+	rows, err := r.db.Query("SELECT Id,Title,Content from posts WHERE Id=$1", id)
 	if err != nil {
 		return models.Post{}, fmt.Errorf("repository: get all posts: %w", err)
 	}
 	var post models.Post
 	for rows.Next() {
-		err := rows.Scan(&post.Title, &post.Content)
+		err := rows.Scan(&post.ID, &post.Title, &post.Content)
 		if err == sql.ErrNoRows {
 			return models.Post{}, errors.New("No posts")
 		} else if err != nil {
 			return models.Post{}, err
 		}
 	}
-	fmt.Println(post)
 	return post, nil
 }
