eaf782f2eadd94b11c48c5081491a4e58a022f83
tursynkhan <tursynkhan001@gmail.com>
2023-01-12 20:33:16 +0600

    add CreateCategoryPost

--- internal/service/post.go
+++ internal/service/post.go
@@ -6,9 +6,10 @@ import (
 )
 
 type Post interface {
-	CreatePost(post models.Post) error
-	GetAllPosts() ([]models.Post, error)
-	GetPost(id int) (models.Post, error)
+	CreatePost(post models.Post) (int, error)
+	GetAllPosts() ([]models.PostInfo, error)
+	GetPost(id int) (models.PostInfo, error)
+	CreatePostCategory(id int, categories []string) error
 }
 
 type PostService struct {
@@ -19,14 +20,18 @@ func NewPostService(repo repository.Post) *PostService {
 	return &PostService{repo: repo}
 }
 
-func (s *PostService) CreatePost(post models.Post) error {
+func (s *PostService) CreatePost(post models.Post) (int, error) {
 	return s.repo.CreatePost(post)
 }
 
-func (s *PostService) GetAllPosts() ([]models.Post, error) {
+func (s *PostService) GetAllPosts() ([]models.PostInfo, error) {
 	return s.repo.GetAllPosts()
 }
 
-func (s *PostService) GetPost(id int) (models.Post, error) {
+func (s *PostService) GetPost(id int) (models.PostInfo, error) {
 	return s.repo.GetPost(id)
+}
+
+func (s *PostService) CreatePostCategory(id int, categories []string) error {
+	return s.repo.CreatePostCategory(id, categories)
 }
