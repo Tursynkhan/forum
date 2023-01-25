18941e79c49d732a30f62491afd32c29d93368dd
tursynkhan <tursynkhan01@gmail.com>
2023-01-25 20:43:42 +0600

    add chain midlleware

--- internal/delivery/middleware.go
+++ internal/delivery/middleware.go
@@ -10,6 +10,23 @@ import (
 	"net/http"
 )
 
+type middleware struct {
+	allMiddlewares []func(http.HandlerFunc) http.HandlerFunc
+}
+
+func (m *middleware) addMidlleware(middle func(http.HandlerFunc) http.HandlerFunc) {
+	m.allMiddlewares = append(m.allMiddlewares, middle)
+}
+
+func (m *middleware) chain(router http.HandlerFunc) http.HandlerFunc {
+	for _, middleware := range m.allMiddlewares {
+		router = middleware(router)
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		router.ServeHTTP(w, r)
+	}
+}
+
 type contextKey string
 
 const key contextKey = "user"
@@ -17,11 +34,15 @@ const key contextKey = "user"
 func (h *Handler) userIdentity(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var user models.User
-		token, err := r.Cookie("session_token")
+		token, err := r.Cookie("token")
 		if err != nil {
-			fmt.Println(err)
+			if err := h.services.DeleteTokenWhenExpireTime(); err != nil {
+				h.errorHandler(w, http.StatusInternalServerError, err.Error())
+				return
+			}
 			if errors.Is(err, http.ErrNoCookie) {
-				next.ServeHTTP(w, r)
+				fmt.Println("error", err)
+				next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), key, models.User{})))
 				return
 			}
 			h.errorHandler(w, http.StatusBadRequest, err.Error())
@@ -30,24 +51,20 @@ func (h *Handler) userIdentity(next http.HandlerFunc) http.HandlerFunc {
 		user, err = h.services.Autorization.ParseToken(token.Value)
 		if err != nil {
 			if errors.Is(err, sql.ErrNoRows) {
-				next.ServeHTTP(w, r)
+				next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), key, models.User{})))
 				return
 			}
 			log.Printf("userIdentity : parse token %v", err)
 			h.errorHandler(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		if err := h.services.Session.CompareExpirationTime(); err != nil {
-			log.Println("middleware : userIdentity: ", err)
-			h.errorHandler(w, http.StatusInternalServerError, err.Error())
-			return
-		}
+
 		ctx := context.WithValue(r.Context(), key, user)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 
-func (h *Handler) secureHeaders(next http.Handler) http.Handler {
+func (h *Handler) secureHeaders(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-XXS-Protection", "1;mode=block")
 		w.Header().Set("X-Frame-Options", "deny")
@@ -55,9 +72,9 @@ func (h *Handler) secureHeaders(next http.Handler) http.Handler {
 	})
 }
 
-func (h *Handler) logRequest(next http.Handler) http.Handler {
+func (h *Handler) logRequest(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.RequestURI)
+		log.Printf("%s\t[%s]\t%s%s", r.Proto, r.Method, r.Host, r.RequestURI)
 		next.ServeHTTP(w, r)
 	})
 }
