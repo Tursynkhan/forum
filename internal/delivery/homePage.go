ded7e97616664628b6dffd9da0be64d7b1b8611d
tursynkhan <tursynkhan01@gmail.com>
2023-01-25 20:43:14 +0600

    update

--- internal/delivery/homePage.go
+++ internal/delivery/homePage.go
@@ -1,14 +1,12 @@
 package delivery
 
 import (
-	"fmt"
 	"forum/internal/models"
 	"log"
 	"net/http"
 )
 
 func (h *Handler) home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("I am in home")
 	var posts []models.PostInfo
 	var err error
 	if r.URL.Path != "/" {
@@ -21,41 +19,7 @@ func (h *Handler) home(w http.ResponseWriter, r *http.Request) {
 		h.errorHandler(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed))
 		return
 	}
-	user, ok := r.Context().Value(key).(models.User)
-	fmt.Println("home:", user)
-	if !ok {
-		categories, err := h.services.GetAllCategories()
-		if err != nil {
-			log.Println("home page : get all categories", err)
-			h.errorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-			return
-		}
-		if len(r.URL.Query()) == 0 {
-			posts, err = h.services.GetAllPosts()
-			if err != nil {
-				log.Println("home page : get all posts : ", err)
-				h.errorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-				return
-			}
-		} else {
-			posts, err = h.services.GetPostByFilter(r.URL.Query())
-			if err != nil {
-				log.Println("home page : GetPostByFilter : ", err)
-				h.errorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-				return
-			}
-		}
-		info := models.Info{
-			Posts:    posts,
-			Category: categories,
-		}
-		if err := h.tmpl.ExecuteTemplate(w, "index.html", info); err != nil {
-			log.Println(err.Error())
-			h.errorHandler(w, http.StatusInternalServerError, err.Error())
-		}
-		return
-	}
-
+	user := r.Context().Value(key).(models.User)
 	categories, err := h.services.GetAllCategories()
 	if err != nil {
 		log.Println("home page : get all categories :", err)
@@ -85,5 +49,6 @@ func (h *Handler) home(w http.ResponseWriter, r *http.Request) {
 	if err := h.tmpl.ExecuteTemplate(w, "index.html", info); err != nil {
 		log.Println(err.Error())
 		h.errorHandler(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 }
