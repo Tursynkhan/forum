13694590b1a73df3502d68ec93a589783793a074
tursynkhan <tursynkhan001@gmail.com>
2023-05-11 18:51:10 +0600

    updated

--- internal/delivery/user.go
+++ internal/delivery/user.go
@@ -1,7 +1,9 @@
 package delivery
 
 import (
+	"errors"
 	"forum/internal/models"
+	"forum/internal/service"
 	"log"
 	"net/http"
 	"strings"
@@ -12,22 +14,29 @@ func (h *Handler) profilePage(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		h.errorHandler(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed))
 	}
-	username := strings.TrimPrefix(r.URL.Path, "/profile")
-	userPage, err := h.services.GetUserByUsername(username)
+	username := strings.TrimPrefix(r.URL.Path, "/profile/")
+	userPage, err := h.services.GetProfileByUsername(username)
 	if err != nil {
-		log.Println("profile : GetUserByUsername", err)
+		log.Println("profile : GetProfileByUsername", err)
 		h.errorHandler(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
 	}
-	posts, err := h.services.GetPostByUsername(username, r.URL.Query())
+	posts, err := h.services.GetPostByUsername(userPage.Username, r.URL.Query())
 	if err != nil {
+		log.Println("user : GetPostByUsername :", err)
+		if errors.Is(err, service.ErrInvalidQuery) {
+			h.errorHandler(w, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+			return
+		}
 		h.errorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
 	info := models.Info{
-		User:  userPage,
-		Posts: posts,
+		User:        user,
+		ProfileUser: userPage,
+		Posts:       posts,
 	}
 	if err := h.tmpl.ExecuteTemplate(w, "profile.html", info); err != nil {
+		log.Println("user : Executetemplate : ", err)
 		h.errorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
