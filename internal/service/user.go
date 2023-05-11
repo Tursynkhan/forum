2d309acb6f10e26e830f5bdfc925c4db5877caf5
tursynkhan <tursynkhan001@gmail.com>
2023-05-11 18:52:38 +0600

    fixed

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,15 +1,16 @@
 package service
 
 import (
-	"fmt"
+	"errors"
 	"forum/internal/models"
 	"forum/internal/repository"
-	"strings"
 )
+
+var ErrInvalidQuery = errors.New("Invalid query request")
 
 type User interface {
 	GetPostByUsername(username string, query map[string][]string) ([]models.PostInfo, error)
-	GetUserByUsername(username string) (models.User, error)
+	GetProfileByUsername(username string) (models.ProfileUser, error)
 }
 
 type UserService struct {
@@ -25,23 +26,47 @@ func (s *UserService) GetPostByUsername(username string, query map[string][]stri
 		posts []models.PostInfo
 		err   error
 	)
-	search, ok := query["posts"]
-	if !ok {
-		return nil, fmt.Errorf("Invalid query request")
+	// search, ok := query["posts"]
+	// if !ok {
+	// 	return nil, fmt.Errorf("service: GetPostByUserName: %w", ErrInvalidQuery)
+	// }
+	for key, val := range query {
+		if key == "posts" {
+			for _, w := range val {
+				if w == "created" {
+					posts, err = s.repo.GetPostByUsername(username)
+					if err != nil {
+						return []models.PostInfo{}, err
+					}
+				} else if w == "liked" {
+					posts, err = s.repo.GetLikedPostByUsername(username)
+					if err != nil {
+						return []models.PostInfo{}, err
+					}
+				} else if w == "commented" {
+					posts, err = s.repo.GetCommentedPostByUsername(username)
+					if err != nil {
+						return []models.PostInfo{}, err
+					}
+				}
+			}
+		} else {
+			return []models.PostInfo{}, err
+		}
 	}
-	switch strings.Join(search, "") {
-	case "created":
-		posts, err = s.repo.GetPostByUsername(username)
-	case "liked":
-		posts, err = s.repo.GetLikedPostByUsername(username)
-	case "commented":
-		posts, err = s.repo.GetCommentPostByUsername(username)
-	default:
-		return nil, fmt.Errorf("service: GetPostByUsernameL %w", err)
-	}
+	// switch strings.Join(search, "") {
+	// case "created":
+	// 	posts, err = s.repo.GetPostByUsername(username)
+	// case "liked":
+	// 	posts, err = s.repo.GetLikedPostByUsername(username)
+	// case "commented":
+	// 	posts, err = s.repo.GetCommentedPostByUsername(username)
+	// default:
+	// 	return nil, fmt.Errorf("service: GetPostByUsernameL %w", err)
+	// }
 	return posts, nil
 }
 
-func (s *UserService) GetUserByUsername(username string) (models.User, error) {
-	return s.repo.GetUserByUsername(username)
+func (s *UserService) GetProfileByUsername(username string) (models.ProfileUser, error) {
+	return s.repo.GetProfileByUsername(username)
 }
